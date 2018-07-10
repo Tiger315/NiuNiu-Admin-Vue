8167006d2f28b7f60720e18339b4fca811c02699
Zhouhu <huhu_315boy@163.com>
2018-07-10 16:24:19 +0800

    初始化项目代码

--- src/components/mail/MailTemplateList.vue
+++ src/components/mail/MailTemplateList.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="MailTemplateList-box" v-cloak>
+		<!--搜索开始-->
 		<div class="add_btn">
 			<el-button type="primary" @click="addMail.centerDialogVisible = true;isEditDialog=1;addMail.addMailTemplate.title='';addMail.addMailTemplate.body=''" size="small">新增邮件模板</el-button>
 		</div>
+		<!--搜索结束-->
+		
 		<!--表格开始-->
 		<el-table v-loading="zLoading" element-loading-text="拼命加载中" :data="mailData" :height="tHeight" stripe style="width: 100%;" empty-text=" " row-key="id">
 			<el-table-column type="index" fixed="left" width="70" :index="typeIndex"></el-table-column>
@@ -17,6 +20,7 @@
 			</el-table-column>
 		</el-table>
 		<!--表格结束-->
+
 		<!--分页开始-->
 		<div style="margin-top: 10px; height: 32px; line-height: 32px; text-align: center;">
 			<span style="float: left; text-align: right; color: #606266; font-size: 14px; padding-top: 3px;">共 {{ zPager.total }} 条</span>
@@ -26,7 +30,7 @@
 		<!--分页结束-->
 
 		<!--对话框开始-->
-		<el-dialog :title="isEditDialog==1? '新增邮件模板' : '编辑邮件模板'" :visible.sync="addMail.centerDialogVisible" width="30%" center>
+		<el-dialog :title="isEditDialog == 1? '新增邮件模板' : '编辑邮件模板'" :visible.sync="addMail.centerDialogVisible" width="30%" center>
 			<div>
 				<el-input placeholder="请输入邮件标题" v-model="addMail.addMailTemplate.title" clearable>
 				</el-input>
@@ -42,194 +46,194 @@
 </template>
 
 <script>
-	import ueditor from '../commen/Ueditor.vue'
-	export default {
-		name: 'MailTemplateList',
-		data() {
-			return {
-				tHeight: document.documentElement.clientHeight - 104,
-				zLoading: true,
-				mailData: [],
-				zPager: {
-					total: 0,
-					size: 30,
-					count: 11,
-					currentPage: 1
-				},
-				zMail: {
-					id: '',
-					title: '',
-					body: ''
-				},
-				zDialog: false,
-				zEditDialog: false,
-				zEditUrl: '#',
-				addMail: {
-					centerDialogVisible: false,
-					addMailTemplate: {
-						title: "",
-						body: "",
-						id: ""
-					}
-				},
-				isEditDialog: 1, //判断弹框是编辑还是新增的，1新增，2编辑
-				clickedIdx:0
-			}
-		},
-		components: {
-			'li-ueditor': ueditor
-		},
-		props: ['defaultValue'],
-		methods: {
-			typeIndex(index) {
-				return index + (this.zPager.currentPage - 1) * this.zPager.size + 1
-			},
-			getMail() {
-				const that = this;
-				that.zLoading = true;
-				let pageNum = that.zPager.currentPage;
-				let pageSize = that.zPager.size;
-				let apiPath = that.apiPath + 'MailTemplate';
-				that.$ajax
-					.get(apiPath)
-					.then(function(response) {
-						let res = response.data;
-						if(res.Code === 1000) {
-							that.mailData = res.Result.Data;
-							that.zPager.total = res.Result.Total;
-						}
-						that.zLoading = false;
-					})
-					.catch(function(response) {})
-			},
-			addMailTemplate() {
-				var addEmailParam = this.addMail.addMailTemplate;
-				addEmailParam.body = this.$refs.ueditor.getUEContent();
-				var that = this;
-				if(!addEmailParam.title) {
-					this.$message({
-						message: '请填写邮件标题！',
-						type: 'warning'
-					});
-					return;
-				} else if(!addEmailParam.body) {
-					this.$message({
-						message: '请填写邮件内容！',
-						type: 'warning'
-					});
-					return;
-				}
-				this.addMail.centerDialogVisible = false;
-				var apiPath = that.apiPath + 'MailTemplate';
-				if(this.isEditDialog == 1) { //新增
-					delete addEmailParam.id;
-					that.$ajax
-						.post(apiPath, addEmailParam)
-						.then(function(response) {
-							let res = response.data;
-							if(res.Code == 1000) {
-								that.mailData.unshift(addEmailParam);
-								that.$message({
-									message: '添加邮件模板成功',
-									type: 'success'
-								});
-							}
-							that.zLoading = false;
-						})
-						.catch(function(response) {})
-				} else if(this.isEditDialog == 2) {
-					that.$ajax
-						.put(apiPath, addEmailParam)
-						.then(function(response) {
-							let res = response.data;
-							if(res.Code == 1000) {
-								that.mailData[that.clickedIdx].title = addEmailParam.title;
-								that.mailData[that.clickedIdx].body = addEmailParam.body;
-								that.$message({
-									message: '修改邮件模板成功',
-									type: 'success'
-								});
-							}
-							that.zLoading = false;
-						})
-						.catch(function(response) {})
-				}
+import ueditor from "../commen/Ueditor.vue";
+export default {
+  name: "MailTemplateList",
+  data() {
+    return {
+      tHeight: document.documentElement.clientHeight - 104,
+      zLoading: true,
+      mailData: [],
+      zPager: {
+        total: 0,
+        size: 30,
+        count: 11,
+        currentPage: 1
+      },
+      zMail: {
+        id: "",
+        title: "",
+        body: ""
+      },
+      zDialog: false,
+      zEditDialog: false,
+      zEditUrl: "#",
+      addMail: {
+        centerDialogVisible: false,
+        addMailTemplate: {
+          title: "",
+          body: "",
+          id: ""
+        }
+      },
+      isEditDialog: 1, //判断弹框是编辑还是新增的，1新增，2编辑
+      clickedIdx: 0
+    };
+  },
+  components: {
+    "li-ueditor": ueditor
+  },
+  props: ["defaultValue"],
+  methods: {
+    typeIndex(index) {
+      return index + (this.zPager.currentPage - 1) * this.zPager.size + 1;
+    },
+    getMail() {
+      const that = this;
+      that.zLoading = true;
+      let pageNum = that.zPager.currentPage;
+      let pageSize = that.zPager.size;
+      let apiPath = that.apiPath + "MailTemplate";
+      that.$ajax
+        .get(apiPath)
+        .then(function(response) {
+          let res = response.data;
+          if (res.Code === 1000) {
+            that.mailData = res.Result.Data;
+            that.zPager.total = res.Result.Total;
+          }
+          that.zLoading = false;
+        })
+        .catch(function(response) {});
+    },
+    addMailTemplate() {
+      var addEmailParam = this.addMail.addMailTemplate;
+      addEmailParam.body = this.$refs.ueditor.getUEContent();
+      var that = this;
+      if (!addEmailParam.title) {
+        this.$message({
+          message: "请填写邮件标题！",
+          type: "warning"
+        });
+        return;
+      } else if (!addEmailParam.body) {
+        this.$message({
+          message: "请填写邮件内容！",
+          type: "warning"
+        });
+        return;
+      }
+      this.addMail.centerDialogVisible = false;
+      var apiPath = that.apiPath + "MailTemplate";
+      if (this.isEditDialog == 1) {
+        //新增
+        delete addEmailParam.id;
+        that.$ajax
+          .post(apiPath, addEmailParam)
+          .then(function(response) {
+            let res = response.data;
+            if (res.Code == 1000) {
+              that.mailData.unshift(addEmailParam);
+              that.$message({
+                message: "添加邮件模板成功",
+                type: "success"
+              });
+            }
+            that.zLoading = false;
+          })
+          .catch(function(response) {});
+      } else if (this.isEditDialog == 2) {
+        that.$ajax
+          .put(apiPath, addEmailParam)
+          .then(function(response) {
+            let res = response.data;
+            if (res.Code == 1000) {
+              that.mailData[that.clickedIdx].title = addEmailParam.title;
+              that.mailData[that.clickedIdx].body = addEmailParam.body;
+              that.$message({
+                message: "修改邮件模板成功",
+                type: "success"
+              });
+            }
+            that.zLoading = false;
+          })
+          .catch(function(response) {});
+      }
+    },
 
-			},
-
-			editMailTemplate(param,index) {
-				this.clickedIdx=index;
-				this.isEditDialog = 2;
-				this.addMail.centerDialogVisible = true;
-				this.addMail.addMailTemplate.title = param.title;
-				this.addMail.addMailTemplate.body = param.body;
-				this.addMail.addMailTemplate.id = param.id;
-			},
-			deleteMailTemplate(param,index){
-				var that=this;
-				var apiPath = that.apiPath + 'MailTemplate/'+param.id;
-				that.$ajax
-						.delete(apiPath)
-						.then(function(response) {
-							let res = response.data;
-							if(res.Code == 1000) {
-								that.mailData.splice(that.clickedIdx,1);
-								that.$message({
-									message: '删除邮件模板成功',
-									type: 'success'
-								});
-							}
-							that.zLoading = false;
-						})
-						.catch(function(response) {})
-			},
-			pagerChange(val) {
-				this.getMail();
-			},
-			seeLawDialog(row) {
-				this.zLoading = true;
-				if(row) {
-					this.zMail.title = row.title;
-					this.zMail.id = row.id;
-					this.zMail.body = row.body;
-				}
-				this.zLoading = false;
-				this.zDialog = true;
-			},
-		},
-		created() {
-			this.getMail();
-		},
-		mounted() {
-			const that = this;
-			window.onresize = () => {
-				return(() => {
-					that.tHeight = document.documentElement.clientHeight - 104;
-				})()
-			}
-		}
-	}
+    editMailTemplate(param, index) {
+      this.clickedIdx = index;
+      this.isEditDialog = 2;
+      this.addMail.centerDialogVisible = true;
+      this.addMail.addMailTemplate.title = param.title;
+      this.addMail.addMailTemplate.body = param.body;
+      this.addMail.addMailTemplate.id = param.id;
+    },
+    deleteMailTemplate(param, index) {
+      var that = this;
+      var apiPath = that.apiPath + "MailTemplate/" + param.id;
+      that.$ajax
+        .delete(apiPath)
+        .then(function(response) {
+          let res = response.data;
+          if (res.Code == 1000) {
+            that.mailData.splice(that.clickedIdx, 1);
+            that.$message({
+              message: "删除邮件模板成功",
+              type: "success"
+            });
+          }
+          that.zLoading = false;
+        })
+        .catch(function(response) {});
+    },
+    pagerChange(val) {
+      this.getMail();
+    },
+    seeLawDialog(row) {
+      this.zLoading = true;
+      if (row) {
+        this.zMail.title = row.title;
+        this.zMail.id = row.id;
+        this.zMail.body = row.body;
+      }
+      this.zLoading = false;
+      this.zDialog = true;
+    }
+  },
+  created() {
+    this.getMail();
+  },
+  mounted() {
+    const that = this;
+    window.onresize = () => {
+      return (() => {
+        that.tHeight = document.documentElement.clientHeight - 104;
+      })();
+    };
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	[v-cloak] {
-		display: none;
-	}
-	
-	html,
-	body {
-		padding: 0;
-		margin: 0;
-		font-size: 14px;
-	}
-	
-	.add_btn {
-		margin: 0 10px 20px;
-		text-align: right;
-	}
-	
-	.mail-textarea {
-		margin-top: 20px;
-	}
+[v-cloak] {
+  display: none;
+}
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-size: 14px;
+}
+
+.add_btn {
+  margin: 0 10px 20px;
+  text-align: right;
+}
+
+.mail-textarea {
+  margin-top: 20px;
+}
 </style>
