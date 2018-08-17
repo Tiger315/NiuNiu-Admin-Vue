8030465171be9fbbfac545a0106419e5403f49e8
fan.ch <530603970@qq.com>
2018-08-17 18:04:54 +0800

    '40周年'

--- static/pages/40thAnniversary/NBD/js/nbd_count.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "../../../../hm.baidu.com/hm.js-de6470f7123b10c2a7885a20733e9cb1"/*tpa=https://hm.baidu.com/hm.js?de6470f7123b10c2a7885a20733e9cb1*/;
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
